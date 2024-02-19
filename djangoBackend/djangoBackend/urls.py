9f1156f15d9efb8d229a6d68d0d869dd9ad1a79d
Isuru Gimhan <isuru.20221485@iit.ac.lk>
2024-02-19 09:18:25 +0530

    fintuned the vgg19 and connect with the back end

--- djangoBackend/djangoBackend/urls.py
+++ djangoBackend/djangoBackend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('treeDetails/',include('treeDetails.urls'))
+    path('treeDetails/',include('treeDetails.urls')),
+    path('TreeIdentification/', include('TreeIdentification.urls'))
 ]

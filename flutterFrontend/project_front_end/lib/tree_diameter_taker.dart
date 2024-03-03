b8babd5b6f56e5d089cdf6ce5a971bc9f79ec37b
Rakhitha Dharmasiri <rakhithaprabashwara@gmail.com>
2024-03-03 21:41:08 +0530

    created treediametertake page and connect to the home page

---
+++ flutterFrontend/project_front_end/lib/tree_diameter_taker.dart
@@ -0,0 +1,100 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: tree_diamter_take(),
+    );
+  }
+}
+
+class tree_diamter_take extends StatefulWidget {
+  @override
+  State<tree_diamter_take> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<tree_diamter_take> {
+  File? selectedImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('image select panel'),
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          selectedImage != null
+              ? Image.file(selectedImage!)
+              : const Text('no image is selected'),
+          ElevatedButton(
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                builder: (BuildContext context) {
+                  return Container(
+                    height: 200,
+                    width: 500,
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // gallery selecting button
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            selectImageFromGallery();
+                          },
+                          icon: const Icon(Icons.photo),
+                          label: const Text('Browse Gallery'),
+                        ),
+                        const Text('OR'),
+                        // camera selecting button
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            captrueImageFromCamera();
+                          },
+                          icon: const Icon(Icons.camera_alt),
+                          label: const Text('Use Camera'),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+            child: const Text('Select image'),
+          ),
+        ]),
+      ),
+    );
+  }
+
+  Future selectImageFromGallery() async {
+    final returnedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (returnedImage == null) return;
+    setState(() {
+      selectedImage = File(returnedImage.path);
+    });
+  }
+
+  Future captrueImageFromCamera() async {
+    final returnedImage =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+
+    if (returnedImage == null) return;
+    setState(() {
+      selectedImage = File(returnedImage.path);
+    });
+  }
+}

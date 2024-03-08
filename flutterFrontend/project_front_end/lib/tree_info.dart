e3c1327533f5c953144a93d61d235bd7714726fa
Isuru Gimhan <isuru.20221485@iit.ac.lk>
2024-03-08 17:10:56 +0530

    update the backend

--- flutterFrontend/project_front_end/lib/tree_info.dart
+++ flutterFrontend/project_front_end/lib/tree_info.dart
@@ -2,6 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DataDisplayPage(),
+    );
+  }
+}
+
 class DataDisplayPage extends StatefulWidget {
   const DataDisplayPage({Key? key}) : super(key: key);
 
